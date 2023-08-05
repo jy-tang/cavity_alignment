78f7a36cd8f35eb40a90a92c2b7ffdc61aa201d9
jy-tang <jytang@stanford.edu>
2023-08-04 23:10:40 -0700

    first complete version

--- diode_simulator/diode_simulator.py
+++ diode_simulator/diode_simulator.py
@@ -1,6 +1,8 @@
 from tools import isotime
 import yaml
 import numpy as np
+from Bragg_mirror_sf import *
+from scipy.stats import skewnorm
 class Diode:
     """
     The main class of a fast diode simulator
@@ -15,13 +17,12 @@ class Diode:
 
         else:   #default number
             self.theta0 = np.pi/4
-            self.beam =  {'sigma_x': 2e-05, 'sigma_y': 2e-05, 'peak_power': 1e-08, 'nx': 100, 'ny': 100, 'napdx': 1000}
-            self.diode = {'response_curve': {'type': 'function', 'skew_num': 1.0, 'noise_level': 0.1},
-                          'position': {'x': 0.0, 'y': 0.0, 'radius': 8e-05}}
+            self.thickness = 200e-6
+            self.beam =  {'dgrid': 0.0001, 'ncar': 100, 'nslice': 50, 'w0': '40e-6', 'Eph': 9831.88, 'npadx': 1000}
+            self.diode_response = {'type': 'function', 'skew_num': 10.0, 'noise_level': 0.05, 't_decay': 10e-9, 'n_sample': 1000}
+            self.diode_position = {'d': 1.0, 'x': 0.0, 'y': 0.0, 'radius': 8e-05}
 
-
-
-
+        self.init_beam()
 
     def parse_input(self, input_file):
         with open('../input/input.yaml') as f:
@@ -29,12 +30,15 @@ class Diode:
         self.check_input_consistency(input)
         self.input = input
         self.theta0 = input['crystal']['theta']/180*np.pi
+        self.thickness = input['crystal']['d']
         self.beam = input['beam']
-        self.diode = input['diode']
+        self.diode_response = input['diode']['response_curve']
+        self.diode_position = input['diode']['position']
+
 
 
     def check_input_consistency(self, input):
-        self.required_inputs = ['beam', 'diode', 'crystal', 'scan']
+        self.required_inputs = ['beam', 'diode', 'crystal']
 
         allowed_params = self.required_inputs + []
         for input_param in input:
@@ -44,7 +48,121 @@ class Diode:
         for req in self.required_inputs:
             assert req in input, f'Required input parameter {req} to {self.__class__.__name__}.__init__(**kwargs) was not found.'
 
-    def initialized_beam(self):
-        sigma_x = self.beam['sigma_x']
-        sigma_y = self.beam['sigma_y']
+    def init_beam(self):
+        """
+        Generate beam in real and k space
+        :return:
+        """
+        dgrid = self.beam['dgrid']
+        ncar = self.beam['ncar']
+        nslice = self.beam['nslice']
+        w0 = self.beam['w0']
+        Eph = self.beam['Eph']
+        self.npadx = self.beam['npadx']
+        self.xlamds = 1.23984198 / Eph * 1e-6
+
+        self.x = np.linspace(-dgrid, dgrid, ncar)
+        self.y = np.linspace(-dgrid, dgrid, ncar)
+        dx = np.mean(np.diff(self.x))
+        dy = np.mean(np.diff(self.y))
+        xmesh, ymesh = np.meshgrid(self.x, self.y, indexing='ij')
+        self.dx, self.dy = dx, dy
+
+        sigma = w0/2
+
+        self.field = 1. / (2. * np.pi * sigma ** 2) * np.exp(
+            -((xmesh) ** 2. / (2. * sigma ** 2.) + (ymesh) ** 2. / (2. * sigma ** 2.)))
+
+        self.field /= np.sqrt(np.sum(np.abs(self.field) ** 2) * dx * dy)
+
+        Dkx = 2 * np.pi / dx
+        Dky = 2 * np.pi / dy
+        nx_padded = ncar + 2 * self.npadx
+        self.kx = Dkx / 2. * np.linspace(-1., 1., nx_padded)
+        self.ky = Dkx / 2. * np.linspace(-1., 1., ncar)
+        self.k = 2*np.pi/self.xlamds
+        self.theta_x = self.kx/self.k
+        self.theta_y = self.ky/self.k
+        # pad in x
+        field_padded = np.pad(self.field, ((self.npadx, self.npadx), (0, 0)))
+
+        # fft in x and y
+        self.fftfld = np.fft.fftshift(np.fft.fft2(field_padded), axes=(0, 1))
+
+
+    def get_Bragg_response(self, d_theta):
+        theta = self.theta0 + d_theta  + self.theta_x
+        Eph = self.beam['Eph']
+        d = self.thickness
+        R0H, R00 = get_Bragg_mirror_response_theta(theta = theta, eph_0=Eph, d=d)
+        return R0H, R00
+
+    def forward_diffraction(self, d_theta):
+        R0H, R00 = self.get_Bragg_response(d_theta)
+        return np.einsum('i,ij->ij', R00, self.fftfld)
+
+
+    def drift(self, fftfld):
+        Ldrift = self.diode_position['d']
+        kx_mesh, ky_mesh = np.meshgrid(self.kx, self.ky, indexing='ij')
+        H = np.exp(-1j * self.xlamds * Ldrift * (kx_mesh ** 2 + ky_mesh ** 2) / (4 * np.pi))
+        return fftfld * H
+
+    def get_diode_response(self, intensity):
+
+        skew_num = self.diode_response['skew_num']
+        noise_level = self.diode_response['noise_level']
+        t_decay = self.diode_response['t_decay']
+        n_sample = self.diode_response['n_sample']
+
+        x = np.linspace(-10, 10, n_sample)
+        y = skewnorm.pdf(x, skew_num)
+        y /= np.max(y)
+        y *= intensity
+        y += np.random.normal(0, noise_level * intensity, len(x))
+        x = (x - np.min(x)) / np.max(x)
+        x *= t_decay
+
+        return x, y
+
+    def get_diode_signal(self, d_theta):
+
+        # transmit the crystal
+        fftfld_transmit = self.forward_diffraction(d_theta)
+        #drift to diode
+        fftfld_transmit = self.drift(fftfld_transmit)
+        #ifft to real space
+        fld_transmit = np.fft.ifft2(np.fft.ifftshift(fftfld_transmit), axes = (0,1))
+        #unpad
+        fld_transmit = fld_transmit[self.npadx:-self.npadx]
+        # through the iris
+        x0 = self.diode_position['x']
+        y0 = self.diode_position['y']
+        r = self.diode_position['radius']
+        xmesh, ymesh = np.meshgrid(self.x, self.y, indexing='ij')
+        fld_transmit[(xmesh - x0) ** 2 + (ymesh - y0) ** 2 > r ** 2] = 0
+        # get final intensity
+        intensity = np.sum(np.abs(fld_transmit)**2)*self.dx*self.dy
+        # get diode response
+        time, signal = self.get_diode_response(intensity)
+        return intensity, time, signal
+
+    def scan_theta(self, d_theta_range):
+        intensity_record = np.zeros(d_theta_range.shape)
+        time_record = np.zeros((len(d_theta_range), self.diode_response['n_sample']))
+        signal_record = np.zeros((len(d_theta_range), self.diode_response['n_sample']))
+
+        for count, d_theta in enumerate(d_theta_range):
+            intensity, time, signal = self.get_diode_signal(d_theta)
+            intensity_record[count] = intensity
+            time_record[count,:] = time
+            signal_record[count,:] = signal
+
+        return intensity_record, time_record, signal_record
+
+
+
+
+
+
 
--- diode_simulator/make_beam.py
+++ diode_simulator/make_beam.py
@@ -1,5 +1,5 @@
 import numpy as np
-def make_gaus_beam(ncar, dgrid=400.e-6, w0=40.e-6, Dt=1e-6 / 3e8, t0=0., nslice=100, trms=2e-6 / 3e8):
+def make_gaus_beam(ncar, dgrid=400.e-6, w0=40.e-6, Dt=1e-6 / 3e8, t0=0., nslice=100, trms=2e-6 / 3e8, energy = 10e-6):
     xs = np.linspace(-1, 1, ncar) * dgrid
     ys = xs
     xv, yv = np.meshgrid(xs, ys, indexing = 'ij')
@@ -11,9 +11,15 @@ def make_gaus_beam(ncar, dgrid=400.e-6, w0=40.e-6, Dt=1e-6 / 3e8, t0=0., nslice=
     ts -= np.mean(ts)
     amps = np.exp(-0.25 * ((ts - t0) / trms) ** 2)
 
+    print(trms)
+
     fld0 = np.zeros([nslice, ncar, ncar]) + 1j * 0.
 
     for ia, a in enumerate(amps):
         fld0[ia] = a * fld
+
+    print(np.sum(np.abs(fld0)**2))
+    print((np.sum(np.abs(fld0)**2)*Dt/nslice))
+    fld0 *= energy/(np.sum(np.abs(fld0)**2)*Dt/nslice)
 
     return fld0
