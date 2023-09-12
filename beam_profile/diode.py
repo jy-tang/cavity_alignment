45b0a93f01f74d1249e60d9d8468e58a8aaf5d4d
jy-tang <53102127+jy-tang@users.noreply.github.com>
2023-09-12 16:01:19 -0700

    a test

--- beam_profile/diode.py
+++ beam_profile/diode.py
@@ -45,7 +45,7 @@ class Diode:
         y = skewnorm.pdf(x, skew_num)
         y /= np.max(y)
         y *= intensity
-        #y += np.random.normal(0, noise_level * intensity, len(x))
+        y += np.random.normal(0, noise_level * intensity, len(x))
 
         x = (x - np.min(x)) / (np.max(x) - np.min(x))
         x *= tsep
